c4bbbd9477c95cfa840d72a5320f5582a029af97
malak <malak>
2019-07-07 10:47:23 +0300

    web_auto refresh code

---
+++ ff/web_auto_refresh/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web auto refresh",
+    'version': "1.0",
+    'author': "Malak Duraidi",
+    'website': "https://www.facebook.com/malak.duraidi",
+    'category': "Sale",
+    'support': "my company",
+    'summary': "Auto refresh (Tree and Kanban view)",
+    'description': "",
+    'license':'LGPL-3',
+    'price': 10.00,
+    'currency': 'EUR',
+    'images':['static/description/icon.png'],
+    'data': [
+        'views/views.xml',
+    ],
+    'depends': ['bus'],
+    'application': True,
+}
